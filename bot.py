8d3c212c793fd5454b6c41677409a9bdac74bb75
foyza <mmirsahidov@gmail.com>
2025-08-22 11:25:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,24 @@
-import joblib
+import asyncio
 from aiogram import Bot, Dispatcher, types
+from deeppavlov import build_model, configs
 import os
 from dotenv import load_dotenv
-import asyncio
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-model = joblib.load("grammar_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
+model = build_model(configs.syntax.syntax_ru, download=True)
 
 @dp.message()
 async def check_grammar(message: types.Message):
-    X = vectorizer.transform([message.text])
-    pred = model.predict(X)[0]
-    conf = model.predict_proba(X)[0][pred]
-    if pred == 1:
-        await message.answer(f"✅ Похоже, предложение грамматичное.\nConfidence: {conf:.2f}")
+    text = message.text
+    corrected = model([text])[0]
+    if text == corrected:
+        await message.answer(f"✅ Предложение грамматично.\nИсправлений нет.")
     else:
-        await message.answer(f"❌ Предложение выглядит неграмматично.\nConfidence: {conf:.2f}")
+        await message.answer(f"❌ Предложение содержит ошибки.\nИсправленная версия:\n{corrected}")
 
 if __name__ == "__main__":
     asyncio.run(dp.start_polling(bot, skip_updates=True))
