1ddf4cf30e7aa6293ea263a47dc9e3ea6843fb9b
foyza <mmirsahidov@gmail.com>
2025-08-22 11:08:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 import asyncio
 
 load_dotenv()
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
@@ -15,16 +14,13 @@ vectorizer = joblib.load("tfidf_vectorizer.pkl")
 
 @dp.message()
 async def check_grammar(message: types.Message):
-    text = message.text
-    X = vectorizer.transform([text])
+    X = vectorizer.transform([message.text])
     pred = model.predict(X)[0]
     conf = model.predict_proba(X)[0][pred]
-
     if pred == 1:
-        reply = f"✅ Похоже, предложение грамматичное.\nConfidence: {conf:.2f}"
+        await message.answer(f"✅ Похоже, предложение грамматичное.\nConfidence: {conf:.2f}")
     else:
-        reply = f"❌ Предложение выглядит неграмматично.\nConfidence: {conf:.2f}"
-    await message.answer(reply)
+        await message.answer(f"❌ Предложение выглядит неграмматично.\nConfidence: {conf:.2f}")
 
 if __name__ == "__main__":
     asyncio.run(dp.start_polling(bot))
